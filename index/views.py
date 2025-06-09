43a85d517876298a76f89d4f9d6a1c56e6b943e2
Vincent-Xinxin04 <1125049348@qq.com>
2025-06-09 23:38:57 +0800

    算法部分

--- index/views.py
+++ index/views.py
@@ -1,11 +1,13 @@
 from http.client import responses
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from algorithm import test
 # Create your views here.
 
 
 def index(request):
     response = HttpResponse('528图书派送系统后端项目')
-
+    a = 1
+    b = 2
+    print(test.add(a,b))
     return response
